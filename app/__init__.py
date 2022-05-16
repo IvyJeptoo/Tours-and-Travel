9c8b9d1965cb69e3a4555a6829b866251200c9d4
charles okunzo <okunzopeter01@gmail.com>
2022-05-16 23:36:49 +0300

    feature --create and register main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from config import config_options
 
 
 
+
 def create_app(config_name):
 
 
@@ -13,6 +14,10 @@ def create_app(config_name):
 
 
   app.config.from_object(config_options[config_name])
+
+
+  from app.main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
 
 
 
