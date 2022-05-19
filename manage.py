c6b110596942f22009f1e1a9047dde3a726e1c5d
charles okunzo <okunzopeter01@gmail.com>
2022-05-19 11:09:31 +0300

    feature --add user login view function

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
-from app import create_app
+from app import create_app, db
+from flask_migrate import Migrate
 
 
 
 app = create_app('development')
 
+migrate = Migrate(app, db)
 
 
 if __name__ == '__main__':
